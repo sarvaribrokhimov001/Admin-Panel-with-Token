77918c64d9ce173c4fc00cb118a1f2935de09dee
ibrokhimovnba <ibrokhimovnba@gmail.com>
2026-05-01 19:04:32 +0500

    Admin Panel with Token

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import React, { useEffect } from 'react'
-import Login from './auth/Login'
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
-import Dashboard from './components/Dashboard'
+import React, { useEffect } from "react";
+import Login from "./auth/Login";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(!token) {
-      navigate("/login")
+    if (!token) {
+      navigate("/login");
+      localStorage.removeItem("token");
     } else {
-      navigate("/dashboard")
+      navigate("/dashboard");
     }
-  } , [token , navigate]);
+  }, [token, navigate]);
 
   return (
-   <Routes>
-      <Route path="/login" element={!token ? <Login /> : <Navigate to="/dashboard" />} />
-      <Route path="/dashboard" element={token ? <Dashboard /> : <Navigate to="/login" />} />
-      <Route path="*" element={<Navigate to={token ? "/dashboard" : "/login"} />} />
-   </Routes>
-  )
-}
-
-export default App
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<h1> Page is Not Founded 😢 </h1>} />
+    </Routes>
+  );
+};
+export default App;
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div>
-        sdjhjs
+        <h1>Dashboard</h1>
     </div>
   )
 }
-
 export default Dashboard
